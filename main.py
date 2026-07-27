851d8cf63ec629e8e1b09b31bac69d92e275cebe
Okunoluwakiikuna Enoch Emmanuel <okunoluwa2019@gmail.com>
2026-07-27 21:27:14 +0200

    Add cash change calculation and denomination breakdown functionality

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-word = "Computer"
-print(word[-3])
+cost = int(input("Cost: R"))
+cash = int(input("Cash: R"))
+
+change = cash - cost
+
+denominations = [200, 100, 50, 20, 10, 5, 2, 1]
+
+for denomination in denominations:
+    while change >= denomination:
+        print(f"R{denomination}")
+        change -= denomination
